0e059d7ef02da9738dd6c8eaaf06ff35c85ee071
allistermugaisi <allistermugaisi@gmail.com>
2021-11-28 19:06:50 +0300

    initial invoice ui commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import styles from './Footer.module.css';
+import FabButton from '../Fab/Fab';
+
+const Footer = () => {
+	const location = useLocation();
+	const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+
+	useEffect(() => {
+		setUser(JSON.parse(localStorage.getItem('profile')));
+	}, [location]);
+
+	return (
+		<footer>
+			<div className={styles.footerText}>
+				© Invoice System| made with ♥ by Allister Mugaisi and Group
+			</div>
+			{user && <FabButton />}
+		</footer>
+	);
+};
+
+export default Footer;
